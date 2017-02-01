dcae372ff1bad3f5bf7c7919875af5b42c81b7cd
Artem <a.t.bolsh@gmail.com>
2017-01-31 23:07:55 -0600

    Modified addArguments method in Main.java. Can now add all negative arguments, as in specification 11.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -14,10 +14,14 @@ public class Main {
     private static int addArguments(String[] args) {
         int r = 0;
         int i = 0;
+        if(args[0].equals("-"))
+            i = 1;
         while(i < args.length){
             r += Integer.valueOf(args[i]);
             i += 1;
         }
+        if(args[0].equals("-"))
+           r = 0 - r;
         return r;
     }
 }
